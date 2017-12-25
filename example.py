300e934f2e9530c1a117a009f21ce48f3a484dfa
Elior Cohen <elior@datascience.co.il>
2017-12-25 11:04:48 +0200

    Updated example.py with parallel call)

--- example.py
+++ example.py
@@ -1,14 +1,24 @@
 import networkx as nx
 from node2vec import Node2Vec
 
+# FILES
+EMBEDDING_FILENAME = './embeddings.emb'
+EMBEDDING_MODEL_FILENAME = './embeddings.model'
+
 # Create a graph
 graph = nx.fast_gnp_random_graph(n=100, p=0.5)
 
 # Precompute probabilities and generate walks
-node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200)
+node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=4)
 
 # Embed
-model = node2vec.fit(window=10, min_count=1, batch_words=4)  # Any keywords acceptable by gensim.Word2Vec can be passed
+model = node2vec.fit(window=10, min_count=1, batch_words=4)  # Any keywords acceptable by gensim.Word2Vec can be passed, `diemnsions` and `workers` are automatically passed (from the Node2Vec constructor)
 
 # Look for most similar nodes
-model.most_similar('2')  # Output node names are always strings
+model.wv.most_similar('2')  # Output node names are always strings
+
+# Save embeddings for later use
+model.wv.save_word2vec_format(EMBEDDING_FILENAME)
+
+# Save model for later use
+model.save(EMBEDDING_MODEL_FILENAME)
