55e945ff40db689ba035ed6c12e1514354477317
pg2455 <pg2455@columbia.edu>
2018-12-23 11:45:01 +0000

    add relevant example

--- example.py
+++ example.py
@@ -10,8 +10,10 @@ graph = nx.fast_gnp_random_graph(n=100, p=0.5)
 
 # Precompute probabilities and generate walks
 node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=4)
-## if d_graph is big enough to fit in the memory, pass tmp_dir which has enough disk space
-node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=4, tmp_dir="/mnt/tmp_data")
+
+## if d_graph is big enough to fit in the memory, pass temp_folder which has enough disk space
+# Note: It will trigger "sharedmem" in Parallel, which will be slow on smaller graphs
+#node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=4, temp_folder="/mnt/tmp_data")
 
 # Embed
 model = node2vec.fit(window=10, min_count=1, batch_words=4)  # Any keywords acceptable by gensim.Word2Vec can be passed, `diemnsions` and `workers` are automatically passed (from the Node2Vec constructor)
